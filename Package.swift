91eb36e4897f3e2ec6193470cec2030932455bca
Alexander Kraev <lexkraev@gmail.com>
2024-01-23 18:32:00 +0300

    add package.swift

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobileNaviKit",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/c-villain/YandexMapsMobileNaviKit/releases/download/4.5.0/YandexMapsMobile.xcframework.zip",
+            checksum: "0e722b7b50c6c18ff2962e213a775fbded991df6277bf3e21710332647055714"
+        ),
+    ]
+)
