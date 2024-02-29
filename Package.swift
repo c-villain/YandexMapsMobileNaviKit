4e79a3450f75021c8fc9a5277a50f689eef86a00
Alexander Kraev <lexkraev@gmail.com>
2024-02-29 23:50:32 +0300

    Update Package.swift to 4.5.1

--- Package.swift
+++ Package.swift
@@ -8,22 +8,17 @@ let package = Package(
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YandexMapsMobile",
             targets: ["YandexMapsMobile"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/c-villain/YandexMapsMobileNaviKit/releases/download/4.5.0/YandexMapsMobile.xcframework.zip",
-            checksum: "b7e5f15f4f29b678a0e25b4bdccb2308ad88aba86f8b44c08ac45eeef941c887"
+            url: "https://github.com/c-villain/YandexMapsMobileNaviKit/releases/download/4.5.1/YandexMapsMobile.xcframework.zip",
+            checksum: "444f13195a0105e03f4ffbe9e53a7e4394e05b22a2ef82ddf6ac86049b68a6c8"
         ),
     ]
 )
